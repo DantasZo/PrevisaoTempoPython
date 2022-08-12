9f6f9ae5ade09b7d4fc34bee7f461c7ba26fb2ab
lucas <dantaslucas337@gmail.com>
2022-08-12 14:35:12 -0300

    Projeto feito com intuito de pesquisar a temperatura em qualquer lugar do mundo

--- Extrator.py
+++ Extrator.py
@@ -1,6 +1,9 @@
+from sys import orig_argv
 import requests
+import chave
 
-API_KEY = "d966860f1a7fa2fdab289bf508f96bc5"
+API_KEY = chave.chave(chamado=orig_argv)
+print(API_KEY)
 
 print("Digite o nome da cidade para consulta")
 cidade = input()
@@ -9,5 +12,5 @@ link = f"https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={API_K
 requisicao = requests.get(link)
 requisicao_dic = requisicao.json()
 descricao = requisicao_dic['weather'][0]['description']
-temperatura = requisicao_dic['main']['temp'] 
+temperatura = requisicao_dic['main']['temp']
 print(descricao, f"{temperatura}°C")
